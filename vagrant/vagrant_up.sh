3a1ba567951b61fcf748a53940dede4c84a82cca
Christian Haudum <christian.haudum@gmail.com>
2015-04-07 10:31:12 +0200

    removed docker containerizer from vagrant setup

--- vagrant/vagrant_up.sh
+++ vagrant/vagrant_up.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 rpm -Uvh http://repos.mesosphere.io/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm
-yum -y install marathon
 yum -y install mesos
 yum -y install mesosphere-zookeeper
-yum -y install docker
+yum -y install marathon
 yum -y install /usr/bin/dig
 yum -y install cyrus-sasl
 yum -y install cyrus-sasl-md5
@@ -12,14 +11,17 @@ yum -y install cyrus-sasl-md5
 
 echo 1 > /var/lib/zookeeper/myid
 echo 'zk://127.0.0.1:2181/mesos' > /etc/mesos/zk
-echo 'docker,mesos' > /etc/mesos-slave/containerizers
+echo 'mesos' > /etc/mesos-slave/containerizers
 echo '10mins' > /etc/mesos-slave/executor_registration_timeout
 echo 'ports(*):[31000-31099, 31101-32000, 4200-4399]' > /etc/mesos-slave/resources
 echo 'crate-demo' > /etc/mesos-master/cluster
-echo '/etc/mesos/passwd' > /etc/mesos-master/credentials
-echo '/etc/mesos/passwd' > /etc/mesos-slave/credential
-echo 'crammd5' > /etc/mesos-master/authenticators
-echo 'crate foo' > /etc/mesos/passwd
+
+# Authentication
+# Comment out and re-run provisioning if you want to test master/slave/framework authentication.
+# echo '/etc/mesos/passwd' > /etc/mesos-master/credentials
+# echo '/etc/mesos/passwd' > /etc/mesos-slave/credential
+# echo 'crammd5' > /etc/mesos-master/authenticators
+# echo 'crate foo' > /etc/mesos/passwd
 
 useradd crate -s /bin/bash -m
 
@@ -38,10 +40,14 @@ cp /vagrant/vagrant/systemd/mesos-dns.service /etc/systemd/system/
 systemctl enable mesos-dns
 systemctl start mesos-dns
 
-gpasswd -a vagrant docker
-systemctl enable docker
-systemctl start docker
+systemctl enable zookeeper
 systemctl start zookeeper
+
+systemctl enable mesos-master
 systemctl start mesos-master
+
+systemctl enable mesos-slave
 systemctl start mesos-slave
+
+systemctl enable marathon
 systemctl start marathon
